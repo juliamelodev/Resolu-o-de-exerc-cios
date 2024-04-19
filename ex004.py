2d33aba7485bd1852da6230e3a7e750155a05b88
Júlia Oliveira Melo <julia@corp.hxbrasil.com.br>
2024-04-19 10:52:07 -0300

    Add ex004.py

--- ex004.py
+++ ex004.py
@@ -1,4 +1,4 @@
 num = int(input('Digite um número: '))
-suces = num + 1
-anteces = num - 1
-print(f'O antecessor e o sucessor é {anteces, suces}')
+s = num + 1
+a = num - 1
+print(f'O antecessor e o sucessor é {a, s}')
